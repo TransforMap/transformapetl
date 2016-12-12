57639f2e5573872f5e5bfb8ffa210bb11c25862f
transformap <collective@transformap.co>
2016-12-12 22:36:58 +0100

    entrypoint and conf example modified

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd /usr/src/app/
 
 if [ ! -f /usr/src/app/transformap.conf ]; then
   echo "Generating configuration"
-  /bin/sh -c "envsubst '$$POSTGRES_USER $$POSTGRES_PASS $$POSTGRES_DB $$POSTGRES_HOST' < /usr/src/app/transformap.example.conf > /usr/src/app/transformap.conf"
+  envsubst '${POSTGRES_USER},${POSTGRES_PASSWORD},${POSTGRES_DB},${POSTGRES_HOST}' < /usr/src/app/transformap.example.conf > /usr/src/app/transformap.conf
 fi
 
 make setupdb
